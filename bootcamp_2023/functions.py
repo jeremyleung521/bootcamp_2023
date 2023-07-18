5011ee35e784dd80069772ae363c557a6db61de6
Jeremy Leung <jml230@pitt.edu>
2023-07-18 11:03:53 -0400

    implement hello_world()

--- bootcamp_2023/functions.py
+++ bootcamp_2023/functions.py
@@ -24,6 +24,29 @@ def canvas(with_attribution=True):
     return quote
 
 
+def hello_world(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format).
+
+    Replace this function and doc string for your own project.
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from.
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution.
+    """
+
+    quote = "Hello World!"
+    if with_attribution:
+        quote += "\n\t- Jeremy Leung"
+    return quote
+
+
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
